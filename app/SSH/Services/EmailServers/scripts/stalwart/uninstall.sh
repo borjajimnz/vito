b551a08db5e39fbce76945e5388fa0444cfa3aac
Borja Jiménez <truestylegs@gmail.com>
2024-08-30 02:07:50 +0200

    add nodejs site type

--- app/SSH/Services/EmailServers/scripts/stalwart/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo systemctl stop stalwart-mail
-sudo systemctl disable stalwart-mail
-sudo rm -Rf /opt/stalwart-mail
-
-echo "Starwalt uninstalled."
---
+++ app/SiteTypes/NodeJs.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\SiteTypes;
+
+use App\Enums\SiteFeature;
+use App\SSH\Services\Webserver\Webserver;
+
+class ReverseProxy extends AbstractSiteType
+{
+    public function language(): string
+    {
+        return 'reverse-proxy';
+    }
+
+    public function supportedFeatures(): array
+    {
+        return [
+            SiteFeature::DEPLOYMENT,
+            SiteFeature::ENV,
+            SiteFeature::SSL,
+            SiteFeature::QUEUES,
+        ];
+    }
+
+    public function createRules(array $input): array
+    {
+        return [
+            'port' => 'required|integer',
+            'auto-installed' => 'sometimes',
+        ];
+    }
+
+    public function createFields(array $input): array
+    {
+        return [
+            'web_directory' => 'public_html',
+        ];
+    }
+
+    public function data(array $input): array
+    {
+        return [
+            'url' => $this->site->getUrl(),
+            'port' => $input['port'],
+            'auto-installed' => $input['auto-installed'] ?? null,
+        ];
+    }
+
+    public function install(): void
+    {
+        /** @var Webserver $webserver */
+        $webserver = $this->site->server->webserver()->handler();
+        $webserver->createVHost($this->site);
+        $this->progress(65);
+    }
+
+    public function editRules(array $input): array
+    {
+        return [
+        ];
+    }
+
+    public function edit(): void
+    {
+        //
+    }
+}
