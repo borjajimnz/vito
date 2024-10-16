5b055fded36490ecb920638dfbec84ae9bb03597
Borja Jiménez <truestylegs@gmail.com>
2024-10-16 22:55:27 +0200

    fix link users

--- app/SSH/Services/Database/scripts/postgresql/link.sh
+++ app/SSH/Services/Database/scripts/postgresql/link.sh
@@ -2,4 +2,9 @@ if ! sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE __database__ TO 
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
+# Otorgar permisos para crear en el esquema público
+if ! sudo -u postgres psql -d __database__ -c "GRANT CREATE ON SCHEMA public TO __username__;"; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
+
 echo "Linking to __database__ finished"
